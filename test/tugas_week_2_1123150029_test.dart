78aa91cc347627e43f0ee7760b6dc2c58c4019de
ENZOUHUY <1123150029@global.ac.id>
2025-09-29 19:07:26 +0700

    remove

--- test/tugas_week_2_1123150029_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tugas_week_2_1123150029/tugas_week_2_1123150029.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
